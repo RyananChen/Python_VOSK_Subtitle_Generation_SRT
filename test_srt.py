cb0e1afec72b586fa29468328e6c36f7373f5a79
RyananChen <40879080+RyananChen@users.noreply.github.com>
2023-04-28 19:06:49 +0800

    重要更新
    今日debug成果
    	• bat方面
    		○ 解决了路径包括感叹号的问题
    			§ 关闭延迟变量即可。
    			§ 关闭延迟变量srt_file在循环内的赋值就会出问题
    			§ 解决办法：在循环外面先对它赋值一下，set srt_file=%%~dpnf.srt
    			§ 然后在循环内写set "srt_file=%%~dpnf.srt" 注意 这里必须有双引号。
    		○ 解决了路径包括&报错的问题
    			§ 把需要加上双引号的地方全都加上了双引号。
    	• python方面
    		○ 解决了文件名包括&的问题，通过io包里面的一个啥功能，把那些玩意全分离出来了，然后就不会因为文件名的&导致虽然能生成文件，但是导不出来了
    		○ 确定不能解决的问题：模型model的路径中文和特殊符号问题，
    			§ 这个的取决于model方法，这个方法又取决于__init__.py里面的定义，它里面估计就是不能进中文和特殊符号，而且不是utf8的问题，因为它的方法里面已经做过一次utf8的转换了，往深了找它是用了一个c的库，叫model.cc，这个库我没找到，懒得找了。
    		○ 做了一个异常处理，比较完善了
    			§ 第一次运行按照配置的来，如果配置出错了会异常，异常捕获：
    				□ 捕获提示，错误了，是否要联网下载，然后会联网下载包。
    				□ 下载过一次之后，根据对应路径是否有文件，第二次运行就可以自动跑下载的那个包了。
    				□ 然后如果把文件全删了，就会再次提示要用英文路径，不能用特殊符号。
    				□ 比较完善的异常处理了算是。

--- test_srt.py
+++ test_srt.py
@@ -1,25 +1,83 @@
-#!/usr/bin/env python3
-
 import subprocess
 import sys
 import os
 
+
+from pathlib import Path
 from vosk import Model, KaldiRecognizer, SetLogLevel
 SAMPLE_RATE = 16000
 SetLogLevel(-1)
-
 # 如果你要自己填写绝对路径，注意python用的是反斜杠
 # model = Model(lang="en-us")
 # model = Model("./model")
-# 我试过了转码，或者别的什么，短时间没找到解决这一行中文error的办法，其他地方都可以中文
+# 我试过了转码，或者别的什么，短时间没找到解决路径中文error的办法，其他地方都可以中文
 # ############模型必须放在没有中文的路径下#########
 # 获取当前脚本所在路径
 script_dir = os.path.dirname(os.path.realpath(__file__))
 # 通过拼接，得出模型文件的绝对路径,此处默认.py脚本和模型文件在同一个文件夹下。
 # 如果要更改中文模型，写为model_path = os.path.join(script_dir, "model_cn")
-model_path = os.path.join(script_dir, "model")
-# 把model的路径作为参数填入Model，创建模型对象
-model = Model(model_path)
+model_path = os.path.join(script_dir, "model") 
+# model_path = model_path.replace('\\', '/') #python自带的路径转移函数,不需要用,model里面带了转义.用了也解决不了中文的问题
+# 联网下载用的参数，想改自己改，链接在https://alphacephei.com/vosk/models/model-list.json，从这里得知的D:\dailyexe\1python3.11\Lib\site-packages\vosk__init__.py
+model_name1 = "vosk-model-en-us-librispeech-0.2"
+lang1 = "en-us"
+
+
+# 执行model = Model(model_name=model_name1, lang=lang1) ，逻辑是
+# 先在本地路径上找，没找到就下载，所以我这边先判断本地这几个路径有没有文件，
+# 没有的话就先执行我们配置的路径，有的话说明路径配置失败了，下载过一次了，那就用下载的
+# 定义默认模型model下载文件夹的路径
+dirs = [Path("/usr/share/vosk"), Path.home() / "AppData/Local/vosk", Path.home() / ".cache/vosk"]
+
+# 遍历文件夹路径列表，检查每个文件夹是否存在文件
+files_exist = any(dir.exists() and any(dir.glob("*")) for dir in dirs)
+dirs_with_files = [str(dir) for dir in dirs if dir.exists() and any(dir.glob("*"))]
+dirs_with_files_str = "\n".join(dirs_with_files)
+
+
+# 根据检查结果执行不同语句，如果有下载的，就用下载的，如果没有下载的，就用我们自己配置的model
+if files_exist:
+    # 执行语句1
+    print("##############################################################") 
+    print("##############################################################") 
+    print("请注意，此时你在用联网下载的模型: " + model_name1 + "\n文件路径在在" + dirs_with_files_str) 
+    print("如果你想运行自己下载的模型，请做到以下两点") 
+    print("1. 清空这几个文件夹的所有模型：/usr/share/vosk，  AppData/Local/vosk，   .cache/vosk \n"+ "    1.1    " + dirs_with_files_str) 
+    print("2. 确保你的模型文件路径没有中文和特殊符号，并和.py脚本在同一路径，命名为model") 
+    print("##############################################################") 
+    print("##############################################################") 
+    try: 
+        model = Model(lang=lang1) 
+    except Exception as e: 
+        print("##############################################################") 
+        print("##############################################################") 
+        print("执行出错了，这是异常信息：", e) 
+        print("##############################################################") 
+        print("##############################################################") 
+else:
+    try: 
+        print("【你在用自己配置正确路径的model】") 
+        # 把model的路径作为参数填入Model，创建模型对象
+        model = Model(model_path) 
+    except Exception as e: 
+        print("##############################################################") 
+        print("##############################################################") 
+        print("执行出错了，这是异常信息：", e) 
+        print("【路径可能有问题】，\n请确认路径中【没有中文】，或者【特殊符号】，\n比如感叹号!或者&等等，\n你可以选择【继续执行】，将会【联网下载一个最小的model】，\n【效果不会太好】\n或者【手动】把路径调整到中文和特殊符号的位置，【再次运行】") 
+        print("##############################################################") 
+        print("##############################################################") 
+        print("【推荐【手动】改好路径，只要没有中文和特殊符号就不会有问题】") 
+        choice = input("是否继续执行，联网下载model？（Y/N）") 
+        if choice.lower() == 'y': 
+                model = Model(lang=lang1) 
+                # model = Model(model_name=model_name1, lang=lang1) 
+        else: 
+            raise
+
+
+
+
+
 
 
 rec = KaldiRecognizer(model, SAMPLE_RATE)
@@ -27,16 +85,26 @@ rec.SetWords(True)
 
 result = []
 
-with subprocess.Popen(["ffmpeg", "-loglevel", "quiet", "-i",
-                            sys.argv[1],
-                            "-ar", str(SAMPLE_RATE) , "-ac", "1", "-f", "s16le", "-"],
-                            stdout=subprocess.PIPE).stdout as stream:
+# 把视频文件的完整路径和文件名赋值给input_path，
+input_path = sys.argv[1]
+# 用os.path.basename()方法获取文件名部分，用os.path.splitext()方法分离出文件名和扩展名。用os.path.dirname()方法获取输入视频文件的路径
+# 最后用os.path.join()方法拼接新的字幕文件路径。这样就能正确地生成字幕文件名,并且避免路径包含特殊符号&就报错的情况。
+output_path = os.path.join(os.path.dirname(input_path), os.path.splitext(os.path.basename(input_path))[0]) + '.srt'
 
-    # print(rec.SrtResult(stream))
-    result.append(rec.SrtResult(stream))
+# 转换生成字幕的主要部分，可以封装成函数，写在另一个py文件了。
+with subprocess.Popen(["ffmpeg", "-loglevel", "quiet", "-i", input_path,
+                        "-ar", str(SAMPLE_RATE) , "-ac", "1", "-f", "s16le", "-"],
+                        stdout=subprocess.PIPE).stdout as stream:
+     # print(rec.SrtResult(stream))
+    result.append(rec.SrtResult(stream).strip())
 
+# 生成完毕了，输出
+print("\n##############################################################################")  
 print(result)
-output = open(sys.argv[1]+'.srt','w')
+print("\n##############################################################################")   
+print("这是" + output_path + "生成字幕文件的内容")   
+print("########################################") 
+output = open(output_path,'w')
 output.write("\n".join(result))
 output.close()
 #写入文件
--- 【复制我】到【中文视频文件夹】双击打开.bat
+++ 【复制我】到【中文视频文件夹】双击打开.bat
@@ -1,67 +1,73 @@
 CHCP 65001
 @echo off
-setlocal enabledelayedexpansion
+setlocal DisableDelayedExpansion
 
-rem 只有这个需要设置，这是test_srt_cn.py的路径。填进去即可。
-set "model_py_dir=D:\dailyexe\1python_VOSK_Github"
+rem 只有这个需要设置，这是test_srt_cn.py的路径。填进去即可。这里不能用set,因为set填中文会报错.
+rem 当然,还是不能用中文,model那边传参好像不能用中文.
+pushd "D:\dailyexe\1python_VOSK_Github"
+
+
+set "model_py_dir=%cd%"
+rem 这应该是正确的"%model_py_dir%"
+popd
 
 rem 如果要用英文字幕生成，把下一行的【test_srt_cn.py】改为【test_srt.py】
 set "py_script=%model_py_dir%\test_srt_cn.py"
-    
+
 set "input_dir=%~dp0"
-set "file_names=%input_dir%\视频目录.txt"
-set source="%model_py_dir%\弹窗提示完成.vbs"
-set destination="%~dp0弹窗提示完成.vbs"
+set "file_names=%input_dir%视频目录.txt"
+set "source=%model_py_dir%\弹窗提示完成.vbs"
+set "destination=%~dp0弹窗提示完成.vbs"
 
 :: 把弹窗提示完成.vbs复制过来，用来删除脚本运行后的bat和txt文件。并起到提醒完成的作用
 rem 
 echo 正在从 "%source%" 复制到 "%destination%"...
-copy %source% %destination%
+copy "%source%" "%destination%"
 echo 【弹窗提示脚本】已拷贝到工作目录下
 
-
-
+::故意写的这一行,没有赋值成功,但是在外面声明了,在for里面就能用,而且不能直接声明空,会出错
+set srt_file=%%~dpnf.srt 
 :: 添加所有同目录以及多级子目录的，mp4文件的绝对路径，到TXT文件中
 pushd "%input_dir%"
 (for /r %%f in (*.mp4) do echo %%~dpnxf) > "%file_names%"
 popd
-
 rem 开始一个一个转换.
-for /f "tokens=*" %%f in (%file_names%) do (
+for /f "tokens=*" %%f in (视频目录.txt) do (
     rem 构造srt文件的文件名
-    set srt_file=%%f.srt
+    set "srt_file=%%~dpnf.srt"
     rem 判断srt文件是否已经存在
-    if exist "!srt_file!" (
+    if exist "%srt_file%" (
         echo 视频【%%f】已有对应.srt字幕文件,跳过该视频。
     ) else (
         echo 【请不要关闭此窗口】,正在转换 %%f ...
-        python "%py_script%" "%%~dpnxf"
+        python "%py_script%" "%%f"
     )
 )
 
+
+goto start
 :: 这一段注释掉，这一段是用来仅仅转换和bat同一个目录下视频用的
 :: 上面一段是转换和bat同一个目录下以及所有子目录下的视频用的
 :: 【只想转换同目录，不转换子目录】的时候可以启用这一段。
-goto start
+set srt_file=%%~dpnf.srt 
 rem 将同一个目录下的所有mp4文件批量添加进同一目录下的file_names.txt
 (for %%f in ("%input_dir%\*.mp4") do echo %%~nxf) > "%file_names%"
 rem 开始一个一个转换.
-for /f "tokens=*" %%f in (%file_names%) do (
+for /f "tokens=*" %%f in (视频目录.txt) do (
     rem 构造srt文件的文件名
-    set srt_file=%%f.srt
+    set "srt_file=%%~dpnf.srt"
+    echo 里面的srt_file %srt_file%
     rem 判断srt文件是否已经存在
-    if exist "!srt_file!" (
-        echo 视频【%%~nxf】已有对应.srt字幕文件,跳过该视频。
+    if exist "%srt_file%" (
+        echo 视频【%%f】已有对应.srt字幕文件,跳过该视频。
     ) else (
         echo 【请不要关闭此窗口】,正在转换 %%f ...
-        python "%py_script%" "%%~nxf"
+        python "%py_script%" "%%f"
     )
 )
 :start
 
-
 echo Done.
-
 cscript /nologo 弹窗提示完成.vbs
 del /f /q 视频目录.txt
 pause
